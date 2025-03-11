f03349a2f4d250acbd4174cbe6a5c8606d91a172
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-11 16:44:03 +0100

    portfolio

--- src/objet/card.js
+++ src/objet/card.js
@@ -1,19 +1,22 @@
+import Image1 from '../src/assets/pokexplorer.png'
+import Image2 from '../src/assets/squide_game.png'
+import Image3 from '../src/assets/relax.png'
 export const Objets=[
     {id:1,
         titre:"squide game",
-        image:"../src/assets/squide_game.png",
+        image:Image2,
         url:"https://taketomi001.github.io/squid_game/",
         description:"Trouver la carte pour participer au jeu squide game",
     },
     {id:2,
         titre:"Pokéxplorer",
-        image:"../src/assets/pokexplorer.png",
+        image:Image1,
         url:"https://wildcodeschool.github.io/2024-12-orleans-cda-project2-team-c/",
         description:"Parcourez la liste des Pokémon avec ce que vous avez trouvé !",
     },
     {id:3,
         titre:"Relax",
-        image:"../src/assets/relax.png",
+        image:Image3,
         url:"https://chloeseclier.github.io/Hackathon2025-TEC/",
         description:"Découvrez votre niveau de stress et voyez ce qu'il vous faut pour vous détendre.",
     },
