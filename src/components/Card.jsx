522a9149e4c2a45c7ba4d6b5d06b3fb2255f5c91
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-11 15:14:21 +0100

    portfolio

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import {Objets} from '../objet/card.js'
 import '../css/card.css'
+const BASE_URL = import.meta.env.BASE_URL;
 export default function Card(){
 
 return(<>
@@ -8,7 +9,7 @@ return(<>
        <div className="cartes-container">
         {Objets.map((objet, index) => (
           <div className="carte">
-      <img src={objet.image} alt={objet.titre} />
+      <img src={{BASE_URL}.objet.image} alt={objet.titre} />
       <div className="carte-content">
         <h2>{objet.titre}</h2>
         <p className='description'>{objet.description}</p>
