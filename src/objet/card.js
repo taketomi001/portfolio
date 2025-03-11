a268fcd04aa89f0afafbf4828b96cd505522b553
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-11 19:40:16 +0100

    porfolio

--- src/objet/card.js
+++ src/objet/card.js
@@ -1,23 +1,28 @@
-import Image1 from '../assets/pokexplorer.png'
-import Image2 from '../assets/squide_game.png'
-import Image3 from '../assets/relax.png'
-export const Objets=[
-    {id:1,
-        titre:"squide game",
-        image:Image2,
-        url:"https://taketomi001.github.io/squid_game/",
-        description:"Trouver la carte pour participer au jeu squide game",
-    },
-    {id:2,
-        titre:"Pokéxplorer",
-        image:Image1,
-        url:"https://wildcodeschool.github.io/2024-12-orleans-cda-project2-team-c/",
-        description:"Parcourez la liste des Pokémon avec ce que vous avez trouvé !",
-    },
-    {id:3,
-        titre:"Relax",
-        image:Image3,
-        url:"https://chloeseclier.github.io/Hackathon2025-TEC/",
-        description:"Découvrez votre niveau de stress et voyez ce qu'il vous faut pour vous détendre.",
-    },
-]
+import Image1 from "../assets/pokexplorer.png";
+import Image2 from "../assets/squide_game.png";
+import Image3 from "../assets/relax.png";
+export const Objets = [
+  {
+    id: 1,
+    titre: "squide game",
+    image: Image2,
+    url: "https://taketomi001.github.io/squid_game/",
+    description: "Trouver la carte pour participer au jeu squide game",
+  },
+  {
+    id: 2,
+    titre: "Pokéxplorer",
+    image: Image1,
+    url: "https://wildcodeschool.github.io/2024-12-orleans-cda-project2-team-c/",
+    description:
+      "Ce projet a été réalisé en groupe dans le cadre d'une formation sur React. Découvrez la liste des Pokémon en explorant les éléments que vous avez appris !",
+  },
+  {
+    id: 3,
+    titre: "Relax",
+    image: Image3,
+    url: "https://chloeseclier.github.io/Hackathon2025-TEC/",
+    description:
+      "Projet de 32h en groupe, dans le but d'un Hackathon. Découvrez votre niveau de stress et voyez ce qu'il vous faut pour vous détendre.",
+  },
+];
