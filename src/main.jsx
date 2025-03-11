93f08fa5578c01d4c7859e74581e80c3ebe64bf1
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-11 12:28:07 +0100

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,26 +6,30 @@ import Home from "./page/Home";
 import AboutMe from "./page/About-me";
 import Error404 from "./page/error404";
 
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about-me", // Changed to use a hyphen
+          element: <AboutMe />,
+        },
+        {
+          path: "*",
+          element: <Error404 />,
+        },
+      ],
+    },
+  ],
   {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about-me",  // Changed to use a hyphen
-        element: <AboutMe />,
-      },
-      {
-        path: '*',
-        element: <Error404 />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
